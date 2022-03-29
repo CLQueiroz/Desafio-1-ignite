2e9d54d1e0924fa83dd1ab0b430c33da196114b2
CLQueiroz <caiqueonisto@live.com>
2022-03-28 22:11:28 -0300

    finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,3 +1,5 @@
+import { v4 as uuid} from 'uuid';
+
 import { useState } from 'react'
 
 import '../styles/tasklist.scss'
@@ -13,17 +15,36 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [idRandom, setIdRandom] = useState(0);
+
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const filteredTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : 
+      task
+    );
+    
+    setTasks(filteredTask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(e => e.id !== id);
+    setTasks( filteredTask);
   }
 
   return (
